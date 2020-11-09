038f5ec54eee3fa94a8c21d928928a98a6e79f80
cassymarie <cassy2205@gmail.com>
2020-11-09 09:20:30 -0700

    commit tree 53a719d3a4daffe6af4422b73a8b884433d56f02

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,32 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
+require 'json'
 
 class Scraper
+  attr_accessor :index, :profile
 
   def self.scrape_index_page(index_url)
+    # index_url = 'https://learn-co-curriculum.github.io/student-scraper-test-page/index.html'
+    index = Nokogiri::HTML(open(index_url))
+    fullClass = []
+    binding.pry
+    index.css(".student-card").each do |person|
+      scraped_students = {
+        :name => person.css("a .card-text-container .student-name").text,
+        :location => person.css("a .card-text-container .student-location").text
+        # :profile_url => person.css(".student-card a").href
+      }
+      # newStudent = Student.new(student)
+      fullClass << newStudent
+      
+    end
+    fullClass
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    @profile = Nokogiri::HTML(index_url)
   end
 
 end
